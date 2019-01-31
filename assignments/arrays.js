065834035f25c07f2053d2e9b97575b89c56c17b
Wsoukkachang <Wsoukkachang@yahoo.com>
2019-01-30 21:35:50 -0600

    new pull request

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -81,7 +81,8 @@ function alphabeticalOrder() {
     let carModels = [''];
 
     for (let i = 0; i < inventory.length; i++) {
-        carModels[i] += inventory[i].car_model;
+        carModels.push(inventory[i].car_model);
+        
     }
 
     let alphabetModel = carModels.sort()
